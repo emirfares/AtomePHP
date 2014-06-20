82dc7c1592a45cd85675c3370e3a6ad8abe73777
emirfares <emir.belmahdi@gmail.com>
2014-06-20 15:56:25 +0100

    Added SQL Files

---
+++ SQL/atome_realm.sql
@@ -0,0 +1,66 @@
+/*
+MySQL Data Transfer
+Source Host: 127.0.0.1
+Source Database: atome_realm
+Target Host: 127.0.0.1
+Target Database: atome_realm
+Date: 20/06/2014 15:56:03
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for ban_ip
+-- ----------------------------
+CREATE TABLE `ban_ip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` varchar(255) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `expire` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `raison` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=124 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for player_accounts
+-- ----------------------------
+CREATE TABLE `player_accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `question` varchar(300) NOT NULL,
+  `reponse` varchar(300) NOT NULL,
+  `subscriptionDate` text NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `pays` varchar(100) NOT NULL,
+  `characters` text NOT NULL,
+  `gifts` text NOT NULL,
+  `gmlevel` tinyint(2) NOT NULL,
+  `banned` tinyint(1) NOT NULL,
+  `points` int(11) NOT NULL,
+  `cadeau` int(11) DEFAULT '0',
+  `lastIP` text,
+  `last_time` text,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=529 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for servers_list
+-- ----------------------------
+CREATE TABLE `servers_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) NOT NULL,
+  `port` int(11) NOT NULL,
+  `system_port` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `player_accounts` VALUES ('509', 'test', 'test', '', '', '', '', '', '', '', '', 'Black-Hair,27', '', '0', '0', '0', '0', null, null);
+INSERT INTO `player_accounts` VALUES ('510', 'emir', 'emir', 'unki', '', '', '', '', '', '', '', 'Xayqto,27|Siypdeki,0', '', '10', '0', '0', '0', null, null);
+INSERT INTO `servers_list` VALUES ('27', '5.168.202.32', '5555', '4040');
