f4886b925a13d825bce79259ed6e6210d764d14a
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:10:20 +0100

    update

--- src/RealmServer/config.php
+++ src/RealmServer/config.php
@@ -1,34 +1,34 @@
 <?php
-namespace RealmServer\Config;
+	namespace RealmServer\Config;
 
-class Config
-{
-	public $sql_informations,
-		   $realm_port, 
-		   $debug, 
-		   $client_version,
-		   $enable_subscription,
-		   $max_players;
-
-	public function __construct()
+	class Config
 	{
-		$this->mysql_informations = array(
-			'host' => 'localhost',
-			'username' => 'root',
-			'password' => null,
-			'db_server' => 'atome',
-			'db_realm' => 'atome_realm',
-			'db_world' => 'atome',
-		);
+		public $sql_informations,
+			   $realm_port, 
+			   $debug, 
+			   $client_version,
+			   $enable_subscription,
+			   $max_players;
 
-		$this->realm_port = 443;
-		$this->debug = true;
-		$this->client_version = "1.29.1";
-		$this->enable_subscription = false;
-		$this->max_players = 200;
+		public function __construct()
+		{
+			$this->mysql_informations = array(
+				'host' => 'localhost',
+				'username' => 'root',
+				'password' => null,
+				'db_server' => 'atome',
+				'db_realm' => 'atome',
+				'db_world' => 'atome',
+			);
+
+			$this->realm_port = 443;
+			$this->debug = true;
+			$this->client_version = "1.29.1";
+			$this->enable_subscription = false;
+			$this->max_players = 200;
+		}
 	}
-}
 
-$config = new Config;
+	$config = new Config;
 
 ?>
