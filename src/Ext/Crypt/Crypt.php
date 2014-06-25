b12d865ead92cc30ab0bd855b4c51db85f5a1b24
emirfares <emir.belmahdi@gmail.com>
2014-06-25 03:40:14 +0100

    Deleted useless functions

--- src/Ext/Crypt/Crypt.php
+++ src/Ext/Crypt/Crypt.php
@@ -24,57 +24,5 @@ class Crypt
         
         return $l7."\n";
     }
-
-    public function plainMD5($hash)
-    {
-        $decryptors = array('Google' => 'http://www.google.com/search?q=%s');
-
-        foreach ($decryptors as $decryptor => $url) 
-        {
-            if (count($hash) > 1)
-            {
-                foreach ($hash as &$one) 
-                {
-                    $one = $this->probeWebMD5($one, $url);
-                }
-            } 
-            else 
-                $hash = $this->probeWebMD5($hash, $url);
-
-            return array($hash, $decryptor);
-        }
-    }
-
-    public function dictionaryAttackMD5($hash, array $wordlist)
-    {
-        $hash = strtolower($hash);
-        foreach ($wordlist as $word) 
-        {
-            if (md5($word) === $hash)
-                return $word;
-        }
-    }
-
-    public function getWordlistMD5($hash, $url)
-    {
-        $list = FALSE;
-        $url = sprintf($url, $hash);
-        if ($response = file_get_contents($url)) 
-        {
-            $list[$response] = 1;
-            $list += array_flip(preg_split('/\s+/', $response));
-            $list += array_flip(preg_split('/(?:\s|\.)+/', $response));
-            $list = array_keys($list);
-        }
-
-        return $list;
-    }
-
-    public function probeWebMD5($hash, $url)
-    {
-        $hash = strtolower($hash);
-        
-        return $this->dictionaryAttackMD5($hash, $this->getWordlistMD5($hash, $url));
-    }
 }
 ?>
