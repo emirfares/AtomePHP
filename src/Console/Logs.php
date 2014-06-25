e34cff3facc442a6a6f877bcb0e28bf1f0c7aa00
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:07:56 +0100

    update

--- src/Console/Logs.php
+++ src/Console/Logs.php
@@ -1,38 +1,46 @@
 <?php
-namespace Console;
-
-class Logs
-{
-	public static function init_logs()
+	namespace Console;
+	/**	@class		Logs
+	 *	@author		Jean Walrave
+	 *	@abstract 	Handling log files & console printing.
+	 */
+	class Logs
 	{
-		$date = date("d-m-Y");
-		$time = date("H:i:s");
-
-		if (!file_exists('logs/'.$date.'.txt'))
+		public static function init_logs()
 		{
-			$log_file = fopen('logs/'.$date.'.txt', "wb");
-			fputs($log_file, "-- Log file created on $date at $time --\n");
-			fclose($log_file);
+			$date = date("d-m-Y");
+			$time = date("H:i:s");
+			# Checking logs dir
+			if (!is_dir('logs')) 
+			{
+				self::print_log('debug','"Logs" dir doesn\'t exist, creating ...');
+   				mkdir('logs');         
+			}
+			if (!file_exists('logs/'.$date.'.txt'))
+			{
+				$log_file = fopen('logs/'.$date.'.txt', "wb");
+				fputs($log_file, "-- Log file created on $date at $time --\n");
+				fclose($log_file);
+			}
+		}
+
+		public static function print_log($log_type, $text, $save = true, $die = false)
+		{
+			global $config;
+
+			$text = "[$log_type] $text \n";
+
+			if ($config->debug AND ($log_type === 'debug' OR $log_type == 'launch'))
+				print( "(".date("H:i:s").") ".$text);
+			else
+				print($text);
+
+			if ($save)
+			{
+				$log_file = fopen('logs/'.date("d-m-Y").'.txt', 'a');
+				fputs($log_file, "(".date("H:i:s").") ".$text);
+				fclose($log_file);
+			}
 		}
 	}
-
-	public static function print_log($log_type, $text, $save = true)
-	{
-		global $config;
-
-		$text = "[$log_type] $text \n";
-
-		if ($config->debug AND ($log_type === 'debug' OR $log_type == 'launch'))
-			print( "(".date("H:i:s").") ".$text);
-		else
-			print($text);
-
-		if ($save)
-		{
-			$log_file = fopen('logs/'.date("d-m-Y").'.txt', 'a');
-			fputs($log_file, "(".date("H:i:s").") ".$text);
-			fclose($log_file);
-		}
-	}
-}
 ?>
