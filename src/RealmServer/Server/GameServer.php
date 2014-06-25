a9f88cd2464ccf9996c5d525e649e93514f2c734
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:10:58 +0100

    update

--- src/RealmServer/Server/GameServer.php
+++ src/RealmServer/Server/GameServer.php
@@ -1,85 +1,81 @@
 <?php
 
-namespace RealmServer\Server;
+	namespace RealmServer\Server;
 
-use Objects\Server as Server;
-use Ext\Bilbon\ORM;
-use PDO;
+	use Objects\Server as Server;
+	use Ext\Bilbon\ORM;
+	use PDO;
 
-class GameServer
-{
-	public $server_list = array();
-	public $db_realm;
-
-	function __construct()
+	class GameServer
 	{
-		global $config;
+		public $server_list = array();
+		public $db_realm;
 
-		ORM\Storage::$instances['realm'] = array(
-	    	'dsn' => 'mysql:host='.$config->mysql_informations['host'].';dbname='.$config->mysql_informations['db_realm'],
-	    	'user' => $config->mysql_informations['username'],
-	    	'password' => $config->mysql_informations['password']
-		);
-
-		$this->db_realm = ORM\Storage::get('realm');
-		
-		$this->loadServers();
-	}
-
-	private function loadServers()
-	{
-		$qGameserver = $this->db_realm->query('SELECT * FROM gameservers');
-		$server = new Server($qGameserver->fetch(PDO::FETCH_ASSOC), $this->db_realm);
-		$server->__set('state', 0);
-
-		$this->server_list[$server->__get('id')] = $server;
-	}
-
-	public function serverExists($id)
-	{
-		foreach ($this->server_list as $server_id => $server)
+		function __construct()
 		{
-			if ($server_id == $id)
+			global $config;
+
+			ORM\Storage::$instances['realm'] = array(
+		    	'dsn' => 'mysql:host='.$config->mysql_informations['host'].';dbname='.$config->mysql_informations['db_realm'],
+		    	'user' => $config->mysql_informations['username'],
+		    	'password' => $config->mysql_informations['password']
+			);
+
+			$this->db_realm = ORM\Storage::get('realm');
+			
+			$this->loadServers();
+		}
+
+		private function loadServers()
+		{
+			$qGameserver = $this->db_realm->query('SELECT * FROM gameservers');
+			$server = new Server($qGameserver->fetch(PDO::FETCH_ASSOC), $this->db_realm);
+			$server->__set('state', 0);
+
+			$this->server_list[$server->__get('id')] = $server;
+		}
+
+		public function serverExists($id)
+		{
+			foreach ($this->server_list as $server_id => $server)
 			{
-				return true;
+				if ($server_id == $id)
+				{
+					return true;
+				}
 			}
+
+			return false;
 		}
 
-		return false;
-	}
-
-	public function getServer($id)
-	{
-		foreach ($this->server_list as $server_id => $server)
+		public function getServer($id)
 		{
-			if ($server_id == $id)
+			foreach ($this->server_list as $server_id => $server)
 			{
-				return $server;
+				if ($server_id == $id)
+				{
+					return $server;
+				}
 			}
+
+			return null;
 		}
 
-		return null;
-	}
-
-	public function parsePacket()
-	{
-		$packet = null;
-		$first = true;
-
-		foreach($this->server_list as $server)
+		public function parsePacket()
 		{
-			if ($first)
-				$first = false;
-			else
-				$packet .= '|';
+			$packet = null;
+			$first = true;
 
-			$packet .= $server->__get('id').';'.$server->__get('state').';'.(75 * $server->__get('id')).';1';
+			foreach($this->server_list as $server)
+			{
+				($first) ? $first = false : $packet .= "|";
+				$packet .= $server->__get('id').';'.$server->__get('state').';'.(75 * $server->__get('id')).';1';
+			}
+
+			return $packet;
 		}
-
-		return $packet;
 	}
-}
 
-$game_servers = new GameServer;
+	$game_servers = new GameServer;
 
 ?>
