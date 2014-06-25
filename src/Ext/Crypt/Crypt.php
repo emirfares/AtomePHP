ba03d473748dff8d0e84ac564477c2e0b2f11f3d
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:09:27 +0100

    update

--- src/Ext/Crypt/Crypt.php
+++ src/Ext/Crypt/Crypt.php
@@ -1,29 +1,29 @@
 <?php
 
-namespace Ext\Crypt;
+    namespace Ext\Crypt;
 
-class Crypt
-{
-    public static function cryptPassword($pass, $key)
+    class Crypt
     {
-        
-        $chars = explode(' ' ,implode(' ',range('a','z')).' '.implode(' ',range('A','Z')).' '.implode(' ',range('0','9')).' - _');
-
-        $l1 = $l2 = $l3 = $l4 = $l5 = 0; 
-        $v1 = $v2 = null;
-        $l7 = '1';
-        for ($l1 = 0; $l1 <= strlen($pass) - 1; $l1++)
+        public static function cryptPassword($pass, $key)
         {
-            $l2 = ord(substr($pass, $l1, 1));
-            $l3 = ord(substr($key, $l1, 1));
-            $l5 = ($l2/16);
-            $l4 = ($l2 % 16);
-            $v1 = $chars[(($l5+$l3) % (count($chars))) % (count($chars))];
-            $v2 = $chars[(($l4+$l3) % (count($chars))) % (count($chars))];
-            $l7 = $l7.$v1.$v2;
+            
+            $chars = explode(' ' ,implode(' ',range('a','z')).' '.implode(' ',range('A','Z')).' '.implode(' ',range('0','9')).' - _');
+
+            $l1 = $l2 = $l3 = $l4 = $l5 = 0; 
+            $v1 = $v2 = null;
+            $l7 = '1';
+            for ($l1 = 0; $l1 <= strlen($pass) - 1; $l1++)
+            {
+                $l2 = ord(substr($pass, $l1, 1));
+                $l3 = ord(substr($key, $l1, 1));
+                $l5 = ($l2/16);
+                $l4 = ($l2 % 16);
+                $v1 = $chars[(($l5+$l3) % (count($chars))) % (count($chars))];
+                $v2 = $chars[(($l4+$l3) % (count($chars))) % (count($chars))];
+                $l7 = $l7.$v1.$v2;
+            }
+            
+            return $l7."\n";
         }
-        
-        return $l7."\n";
     }
-}
 ?>
