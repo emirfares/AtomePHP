c4d486140de0fe97ec57b11cdb51fff399c9967d
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:09:46 +0100

    update

--- src/Ext/Crypt/Random.php
+++ src/Ext/Crypt/Random.php
@@ -6,15 +6,13 @@ class Random
 {
 	public static function generateKey($size = 8)
 	{
-		$chars = explode(' ' ,implode(' ',range('a','z')).' '.implode(' ',range('A','Z')).' '.implode(' ',range('0','9')).' - _');
-	    $count = mb_strlen($chars);
+		$chars = explode(" ",implode(" ",range('a','z'))." ".implode(" ",range('A','Z'))." ".implode(" ",range('0','9'))." - _");
+    	$key = "";
 
-	    for ($i = 0, $str = ''; $i < $size; $i++) 
-	    {
-	        $index = rand(0, $count - 1);
-	        $str .= mb_substr($chars, $index, 1);
-	    }
-
-	    return $str;
+    	for ($i=1; $i <= count($chars) ; $i++) 
+        { 
+    		$key .= $chars[rand(0,count($chars) - 1)];
+    	}
+    	return $key;
 	}
 }
