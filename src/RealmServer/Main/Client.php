1000f36401e658fe58f418a38378af562a766a54
emirfares <emir.belmahdi@gmail.com>
2014-06-25 18:10:39 +0100

    update

--- src/RealmServer/Main/Client.php
+++ src/RealmServer/Main/Client.php
@@ -1,177 +1,217 @@
 <?php
 
-namespace RealmServer\Main;
+	namespace RealmServer\Main;
 
-use Ext\Bilbon\ORM as ORM;
-use Ext\Crypt as Crypt;
+	use Ext\Bilbon\ORM as ORM;
+	use Ext\Crypt as Crypt;
 
-use Console\Logs as Logs;
-use RealmServer\Realm as Realm;
-use Worker;
+	use Console\Logs as Logs;
+	use RealmServer\Realm as Realm;
+	use Worker;
 
-class Client extends Worker 
-{
-	public static $db_realm;
-	public $key;
-	public $state;
-	public $account = array();
-
-	public function __construct($socket)
+	/**	@class		Client
+	 *	@author		Emir Fares BELMAHDI & Jean Walrave
+	 *	@abstract 	Handling new clients & parsing socket connections.
+	 */
+	class Client extends Worker 
 	{
-		global $config;
+		/**	@var		dbrealm
+		 *	@abstract	ressource - The client's PDO Connection.
+		 */
+		public static $db_realm;
 
-		$this->__set('config', $config);
-		$this->__set('socket', $socket);
-		$this->__set('key', Crypt\Random::generateKey());
-		socket_getpeername($this->socket, $ip);
-		$this->__set('ip', $ip);
-		$this->__set('hostname', gethostbyaddr($this->ip));
+		/**	@var		key
+		 *	@abstract	string - The client's encryption key.
+		 */
+		public $key;
 
-		$this->start(PTHREADS_INHERIT_ALL | PTHREADS_ALLOW_GLOBALS);
-	}
+		/**	@var		state
+		 *	@abstract	string - The client's packet state.
+		 */
+		public $state;
 
-    public function __get($property) 
-    {
-        return isset($this->$property) ? $this->$property : null;
-    }
-    
-    public function __set($property, $value) 
-    {
-        $this->$property = $value;
-    }
+		/**	@var		infos
+		 *	@abstract	ressource - The client's account informations.
+		 */
+		public $account = array();
 
-	public function send($buffer)
-	{
-		socket_write($this->socket, $buffer.chr(0));
-		Logs::print_log('debug', "Socket send {$buffer}", false);
-	}
-
-	public function run()
-	{
-		$this->__set('state', 'version_checking');
-		Logs::print_log('debug', "New client added ({$this->key}), version checking...", false);
-		$this->send('HC'.$this->key);
-		$buff = null;
-
-		do 
+		/**	@function	__construct
+		 *	@abstract	Launches the Thread and start parsing packets.
+		 *	@param		ressource - Socket ressource used to communicate with the client.
+		 *	@result		void
+		 */
+		public function __construct($socket)
 		{
-			$buffer = socket_read($this->socket, 1024);
-				
-			if (empty($buffer))
+			global $config;
+
+			$this->__set('config', $config);
+			$this->__set('socket', $socket);
+			$this->__set('key', Crypt\Random::generateKey());
+			socket_getpeername($this->socket, $ip);
+			$this->__set('ip', $ip);
+			$this->__set('hostname', gethostbyaddr($this->ip));
+
+			$this->start(PTHREADS_INHERIT_ALL | PTHREADS_ALLOW_GLOBALS);
+		}
+
+	    public function __get($property) 
+	    {
+	        return isset($this->$property) ? $this->$property : null;
+	    }
+	    
+	    public function __set($property, $value) 
+	    {
+	        $this->$property = $value;
+	    }
+
+	    /**	@function	send
+		 *	@abstract	Sends a packet to the client.
+		 *	@param		string - Packet to send.
+		 *	@result		void
+		 */
+		public function send($buffer)
+		{
+			socket_write($this->socket, $buffer.chr(0));
+			Logs::print_log('debug', "Sent -> {$buffer}", false);
+		}
+
+		/**	@function	run
+		 *	@abstract	Used by pThreads to run the new thread.
+		 *	@result		void
+		 */
+		public function run()
+		{
+			$this->__set('state', 'version_checking');
+			Logs::print_log('debug', "New client added ({$this->key}), version checking...", false);
+			$this->send('HC'.$this->key);
+			$buff = null;
+
+			do 
 			{
-				$this->disconnect();
+				$buffer = socket_read($this->socket, 1024);
+					
+				if (empty($buffer))
+					$this->disconnect();
+
+				$buffer = (str_replace(chr(10), null, $buffer));
+
+				for ($i = 0; $i < strlen($buffer); $i++) 
+				{ 
+				    if ($buffer[$i] != chr(0)) 
+				 		$buff .= $buffer[$i];
+				    else
+				    {
+				    	Logs::print_log('debug', "Receved <- {$buff}", false);
+				    	$this->parsePacket(trim($buff));
+				    	$buff = null;
+				    }  	
+				}
 			}
+			while($buffer != null);
+		}
 
-			$buffer = (str_replace(chr(10), null, $buffer));
-
-			for ($i = 0; $i < strlen($buffer); $i++) 
-			{ 
-			    if ($buffer[$i] != chr(0)) 
-			 		$buff .= $buffer[$i];
-			    else
-			    {
-			    	Logs::print_log('debug', "Socket recieved {$buff}", false);
-			    	$this->parsePacket(trim($buff));
-			    	$buff = null;
-			    }  	
-			}
-		} 
-
-		while($buffer != null);
-	}
-
-	private function parsePacket($packet)
-	{
-		switch($this->__get('state'))
+		/**  @function	parsePacket
+		 *	@abstract	Parses the packet receved from the client.
+		 *	@param		string - Packet receved.
+		 *	@result		void
+		 */
+		private function parsePacket($packet)
 		{
-			case 'version_checking' :
-				if ($packet == $this->config->client_version) 
+			switch($this->__get('state'))
+			{
+				case 'version_checking' :
+					if ($packet == $this->config->client_version) 
+					{
+						$this->__set('state', 'account_checking');
+						Logs::print_log('debug', 'Version validated, account verification...', false);
+					}
+					else 
+					{
+						$this->send('AlEv'.$this->config->client_version);
+						Logs::print_log('debug', "Wrong version ({$packet}) form client {$this->key} {$this->ip}");
+						$this->__set('state', null);
+						$this->disconnect();
+					}
+				break;
+
+				case 'account_checking':
+					$this->parseAccount($packet);
+				break;
+
+				case 'base':
+					$this->parseBase($packet);
+				break;
+			}
+		}
+
+		/**	@function	parseAccount
+		 *	@abstract	Parses the account informations packet receved from the client.
+		 *	@param		string - Packet receved.
+		 *	@result		void
+		 */
+		private function parseAccount($packet) 
+		{
+			global $game_servers;
+			global $config;
+
+			ORM\Storage::$instances['realm'] = array(
+		    	'dsn' => 'mysql:host='.$config->mysql_informations['host'].';dbname='.$config->mysql_informations['db_realm'],
+		    	'user' => $config->mysql_informations['username'],
+		    	'password' => $config->mysql_informations['password']
+			);
+
+			self::$db_realm = ORM\Storage::get('realm');
+
+			$pack = explode('#', $packet);
+			$username = strtolower($pack[0]);
+
+			$account = new Realm\RealmClient($username);
+			$account = $account->account;
+
+			if ($account != null)
+			{
+				$tpass = trim(Crypt\Crypt::cryptPassword($account->__get('password'), $this->key));
+
+				if ($username == strtolower($account->__get('username')) && $tpass == trim($pack[1])) 
 				{
-					$this->__set('state', 'account_checking');
-					Logs::print_log('debug', 'Version validated, account verification...', false);
+					if (!$account->__get('is_banned'))
+					{
+						$this->account = $account->__getAll();
+
+						$this->send('Ad'.$account->__get('nickname'));
+						$this->send('Ac0'); //TODO : Communities, Default (0) Francophone 
+						$this->send('AH'.$game_servers->parsePacket());
+						$this->send(($account->__get('gmlevel') > 0) ? 'Alk1' : 'Alk0');
+						$this->send('AQ');
+
+						$this->__set('state', 'base');
+						Logs::print_log('debug', "Client {$account->__get('nickname')} connected !");
+					}
+					else
+					{
+						$this->send('AlEb');
+						$this->__set('state', null);
+						Logs::print_log('debug', "Client {$account->__get('nickname')} is banned, connection refused !");
+					}
 				}
 				else 
 				{
-					$this->send('AlEv'.$this->config->client_version);
-					Logs::print_log('debug', "Wrong version ({$packet}) form client {$this->key}", true);
 					$this->__set('state', null);
-					$this->disconnect();
-				}
-			break;
-
-			case 'account_checking':
-				$this->parseAccount($packet);
-			break;
-
-			case 'base':
-				$this->parseBase($packet);
-			break;
-		}
-	}
-
-	private function parseAccount($packet) 
-	{
-		global $game_servers;
-		global $config;
-
-		ORM\Storage::$instances['realm'] = array(
-	    	'dsn' => 'mysql:host='.$config->mysql_informations['host'].';dbname='.$config->mysql_informations['db_realm'],
-	    	'user' => $config->mysql_informations['username'],
-	    	'password' => $config->mysql_informations['password']
-		);
-
-		self::$db_realm = ORM\Storage::get('realm');
-
-		$pack = explode('#', $packet);
-		$username = strtolower($pack[0]);
-
-		$account = new Realm\RealmClient($username);
-		$account = $account->account;
-
-		if ($account != null)
-		{
-			$tpass = trim(Crypt\Crypt::cryptPassword($account->__get('password'), $this->key));
-
-			if ($username == strtolower($account->__get('username')) && $tpass == trim($pack[1])) 
-			{
-				if (!$account->__get('is_banned'))
-				{
-					$this->account = $account->__getAll();
-
-					$this->send('Ad'.$account->__get('nickname'));
-					$this->send('Ac0');
-					$this->send('AH'.$game_servers->parsePacket());
-					$this->send(($account->__get('gmlevel') > 0) ? 'Alk1' : 'Alk0');
-					$this->send('AQ');
-
-					$this->__set('state', 'base');
-					Logs::print_log('debug', "Client {$account->__get('nickname')} connected !", true);
-				}
-				else
-				{
-					$this->send('AlEb');
-					$this->__set('state', null);
-					Logs::print_log('debug', "Client {$account->__get('nickname')} is banned, connection refused !", true);
+					$this->send('AlEf');
 				}
 			}
-			else 
+			else
 			{
 				$this->__set('state', null);
 				$this->send('AlEf');
 			}
 		}
-		else
-		{
-			$this->__set('state', null);
-			$this->send('AlEf');
-		}
-	}
 
-		/*!	@function	sendCharactersList
-			@abstract	Sends remaining subscription time and the list of client's characters.
-			@param		string - Packet receved.
-			@result		void
-		*/
+		/**	@function	sendCharactersList
+		 *	@abstract	Sends remaining subscription time and the list of client's characters.
+		 *	@param		string - Packet receved.
+		 *	@result		void
+		 */
 		private function sendCharactersList() 
 		{
 			global $game_servers;
@@ -200,11 +240,11 @@ class Client extends Worker
 			$this->send($packet);	
 		}
 
-		/*!	@function	parseCommand
-			@abstract	Parses admin commands sent using the game admin console.
-			@param		string - Packet receved.
-			@result		void
-		*/
+		/**	@function	parseCommand
+		 *	@abstract	Parses admin commands sent using the game admin console.
+		 *	@param		string - Packet receved.
+		 *	@result		void
+		 */
 		private function parseCommand($packet)
 		{
 			$p = explode(" ",$packet);
@@ -220,21 +260,21 @@ class Client extends Worker
 			}
 		}
 
-		/*!	@function	parseGameServer
-			@abstract	Parses game server redirection request.
-			@param		integer - server ID.
-			@result		void
-		*/
+		/**	@function	parseGameServer
+		 *	@abstract	Parses game server redirection request.
+		 *	@param		integer - server ID.
+		 *	@result		void
+		 */
 		private function parseGameServer($id)
 		{
 
 		}
 
-		/*!	@function	parseBase
-			@abstract	Parses Base packets  (Packets sent after successful login).
-			@param		string - Packet receved.
-			@result		void
-		*/
+		/**	@function	parseBase
+		 *	@abstract	Parses Base packets  (Packets sent after successful login).
+		 *	@param		string - Packet receved.
+		 *	@result		void
+		 */
 		private function parseBase($packet)
 		{
 			switch (substr($packet, 0,2))
@@ -257,14 +297,14 @@ class Client extends Worker
 			}
 		}
 
-		/*!	@function	disconnect
-			@abstract	Disconnects the client.
-			@result		void
-		*/
+		/**	@function	disconnect
+		 *	@abstract	Disconnects the client.
+		 *	@result		void
+		 */
 		public function disconnect()
 		{
 			socket_close($this->socket);
-			print_info('Client disconnected');
+			//print_info('Client disconnected');
 			$this->shutdown();
 		}
 	}
