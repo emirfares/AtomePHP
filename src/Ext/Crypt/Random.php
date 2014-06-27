f0b9f54f717edd475c79762ffbeae4b14e191e70
emirfares <emir.belmahdi@gmail.com>
2014-06-27 01:01:50 +0100

    Corrected "generateKey()" method

--- src/Ext/Crypt/Random.php
+++ src/Ext/Crypt/Random.php
@@ -4,12 +4,14 @@ namespace Ext\Crypt;
 
 class Random
 {
-	public static function generateKey($size = 8)
+	public static function generateKey($size = 64)
 	{
 		$chars = explode(" ",implode(" ",range('a','z'))." ".implode(" ",range('A','Z'))." ".implode(" ",range('0','9'))." - _");
     	$key = "";
+        if($size > ($s = count($chars)))
+            $size = $s;
 
-    	for ($i=1; $i <= count($chars) ; $i++) 
+    	for ($i=1; $i <= $size ; $i++) 
         { 
     		$key .= $chars[rand(0,count($chars) - 1)];
     	}
