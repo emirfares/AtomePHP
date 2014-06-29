b69390bbb7389d8d5c542be463f2950b256bf2f3
emirfares <emir.belmahdi@gmail.com>
2014-06-29 12:22:47 +0100

    New config file

--- src/RealmServer/config.php
+++ src/RealmServer/config.php
@@ -1,34 +1,17 @@
 <?php
 	namespace RealmServer\Config;
 
-	class Config
-	{
-		public $sql_informations,
-			   $realm_port, 
-			   $debug, 
-			   $client_version,
-			   $enable_subscription,
-			   $max_players;
+	define('MYSQL_HOST', 'localhost');
+	define('MYSQL_USERNAME', 'root');
+	define('MYSQL_PASSWORD', null);
+	define('DB_REALM', 'atome');
+	define('DB_SERVER', 'atome');
+	define('DB_WORLD', 'atome');
+	define('REALM_PORT', 443);
+	define('DEBUG', true);
+	define('CLIENT_VERSION', '1.29.1');
+	define('ENABLE_SUBSCRIPTION', false);
+	define('MAX_PLAYERS', 200);
 
-		public function __construct()
-		{
-			$this->mysql_informations = array(
-				'host' => 'localhost',
-				'username' => 'root',
-				'password' => null,
-				'db_server' => 'atome',
-				'db_realm' => 'atome',
-				'db_world' => 'atome',
-			);
-
-			$this->realm_port = 443;
-			$this->debug = true;
-			$this->client_version = "1.29.1";
-			$this->enable_subscription = false;
-			$this->max_players = 200;
-		}
-	}
-
-	$config = new Config;
-
+	ini_set('date.timezone', 'Europe/Paris');
 ?>
