f96278a06d759056df108459b7493065f95c8914
emirfares <emir.belmahdi@gmail.com>
2014-06-30 04:19:10 +0100

    Added some informations

--- src/RealmServer/config.php
+++ src/RealmServer/config.php
@@ -1,17 +1,28 @@
 <?php
 	namespace RealmServer\Config;
 
+	#Database server adress
 	define('MYSQL_HOST', 'localhost');
+	#Database username
 	define('MYSQL_USERNAME', 'root');
-	define('MYSQL_PASSWORD', null);
+	#Database password
+	define('MYSQL_PASSWORD', NULL);
+	#Realm database name
 	define('DB_REALM', 'atome');
+	#Server database name
 	define('DB_SERVER', 'atome');
+	#World database name
 	define('DB_WORLD', 'atome');
+	#Game port (port number to listen)
 	define('REALM_PORT', 443);
-	define('DEBUG', true);
+	#Activate debug mode ? (TRUE = yes | FALSE = no)
+	define('DEBUG', TRUE);
+	#Client version
 	define('CLIENT_VERSION', '1.29.1');
-	define('ENABLE_SUBSCRIPTION', false);
+	#Enable subscription ? (TRUE = yes | FALSE = no)
+	define('ENABLE_SUBSCRIPTION', TRUE);
+	#Maximum number of simultaneous clients allowed
 	define('MAX_PLAYERS', 200);
-
+	#Timezone
 	ini_set('date.timezone', 'Africa/Algiers');
 ?>
